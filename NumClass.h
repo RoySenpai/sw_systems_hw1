1a74d5a594a61468a51a4a9b694b47ea7730cc7a
Roy <me@hiyorix.com>
2022-11-10 08:46:38 +0200

    Update the header file and basic libary functions

--- NumClass.h
+++ NumClass.h
@@ -1,20 +1,31 @@
+#ifndef _NUMCLASS_H
+#define _NUMCLASS_H
+
 #define TRUE 1
 #define FALSE 0
 
-/* will return if a number is Armstrong number
-An Armstrong number is an n-digit number that is equal to the sum of the nth powers of its digits.
-For Example: 407 = 43 + 03 + 73 = 64 + 0 + 343 = 407
+/* 
+ * Will return if a number is Armstrong number.
+ * An Armstrong number is an n-digit number that is equal to the sum of the nth powers of its digits.
+ * For Example: 407 = 43 + 03 + 73 = 64 + 0 + 343 = 407
 */
 int isArmstrong(int);
 
-/* will return if a number is a palindrome */
+/* 
+ * Will return if a number is a palindrome.
+*/
 int isPalindrome(int);
 
-/* will return if a number is prime*/
+/* 
+ * Will return if a number is prime
+*/
 int isPrime(int);
 
-/*Strong number is a special number whose sum of the factorial of digits is equal to the original
-number. For Example: 145 is strong number
-1! + 4! + 5! = 145
+/*
+ * Strong number is a special number whose sum of the factorial of digits is equal to the original
+ * number. For Example: 145 is strong number
+ * 1! + 4! + 5! = 145
 */
 int isStrong(int);
+
+#endif
--- basicClassification.c
+++ basicClassification.c
@@ -1,9 +1,35 @@
 #include "NumClass.h"
 
-int isPrime(int x) {
+long factorial(int);
 
+int isPrime(int x) {
+    int pos;
+
+    for (pos = 2; pos < sqrt(x); ++pos)
+    {
+        if (x % pos == 0)
+            return FALSE;
+    }
+
+    return TRUE;
 }
 
 int isStrong(int x) {
-    
+    int tmp_res = x;
+    int fac_sum = 0;
+
+    while (temp_res > 0)
+    {
+        fac_sum = factorial((temp_res % 10));
+        temp_res /= 10;
+    }
+
+    return ((fac_sum == x) ? TRUE:FALSE);
+}
+
+long factorial(int x) {
+    if (x == 0)
+        return 1;
+
+    return x + factorial(x-1);
 }
