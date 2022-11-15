fc661061676db3628f0381ed11124bd979c4e282
Roy <me@hiyorix.com>
2022-11-15 16:18:01 +0200

    Fixing handeling with 0's and 1's input

--- basicClassification.c
+++ basicClassification.c
@@ -6,6 +6,10 @@ long factorial(int);
 int isPrime(int x) {
     int pos;
 
+    // 0 & 1 aren't primes!
+    if (x == 0 || x == 1)
+        return FALSE;
+
     for (pos = 2; pos <= sqrt(x); ++pos)
     {
         if (x % pos == 0)
@@ -18,6 +22,9 @@ int isPrime(int x) {
 int isStrong(int x) {
     int tmp_res = x;
     int fac_sum = 0;
+
+    if (!x)
+        return FALSE;
 
     while (tmp_res > 0)
     {
